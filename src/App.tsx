a059754cd585b42cfc998cecc236146f8f2531db
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-25 16:39:04 -0300

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GlobalStyle from "./styles/global";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
+import ToastContainer from "./components/ToastContainer";
 import { AuthProvider } from "./hooks/AuthContext";
 
 const App: React.FC = () => (
@@ -12,6 +13,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
   </>
 );
 export default App;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -45,8 +45,10 @@ const SignIn: React.FC = () => {
 
         signIn({ email: data.email, password: data.password });
       } catch (err) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn]
