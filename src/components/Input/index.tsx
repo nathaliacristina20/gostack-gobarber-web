e0d979018b2e2aec0930c3fad712983811c79669
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-25 11:50:27 -0300

    Exibindo erros no input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -48,6 +48,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
+      {error}
     </Container>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,18 +1,24 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 
 import { FiArrowLeft, FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Form } from "@unform/web";
 import * as Yup from "yup";
+import { FormHandles } from "@unform/core";
 import logoImg from "../../assets/logo.svg";
-
 import { Container, Content, Background } from "./styles";
+
+import getValidationErrors from "../../utils/getValidationErrors";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required("Nome obrigatório"),
         email: Yup.string()
@@ -25,9 +31,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
     }
-    console.log(data);
   }, []);
 
   return (
@@ -36,7 +42,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form action="" onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro </h1>
           <Input type="text" name="name" icon={FiUser} placeholder="Nome" />
           <Input type="text" name="email" icon={FiMail} placeholder="E-mail" />
