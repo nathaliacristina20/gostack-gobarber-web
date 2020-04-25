e7c7051184a310a728d2fd3721158dc47e6fec01
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-25 12:52:09 -0300

    Validação de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import * as Yup from "yup";
+import { FormHandles } from "@unform/core";
 import logoImg from "../../assets/logo.svg";
 
 import { Container, Content, Background } from "./styles";
 
+import getValidationErrors from "../../utils/getValidationErrors";
+
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-const SignIn: React.FC = () => (
-  <>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().required("Senha obrigatória"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form action="">
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
 
           <Input type="text" name="email" icon={FiMail} placeholder="E-mail" />
@@ -27,7 +54,7 @@ const SignIn: React.FC = () => (
 
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="account">
           <FiLogIn />
@@ -36,6 +63,6 @@ const SignIn: React.FC = () => (
       </Content>
       <Background />
     </Container>
-  </>
-);
+  );
+};
 export default SignIn;
