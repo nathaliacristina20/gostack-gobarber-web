0fc8fa5e784d779cd07de7295891a160302f3d89
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-25 00:15:02 -0300

    Pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import GlobalStyle from "./styles/global";
+import SignIn from "./pages/SignIn";
 
 const App: React.FC = () => (
   <>
-    <p>Works!</p>
+    <SignIn />
     <GlobalStyle />
   </>
 );
