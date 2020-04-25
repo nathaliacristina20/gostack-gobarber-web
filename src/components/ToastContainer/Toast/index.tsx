ff2fef4f4f506458707bc99d61741da34be4630e
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-25 17:57:20 -0300

    Animando Toasts

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -11,6 +11,7 @@ import { ToastMessage, useToast } from "../../../hooks/toast";
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -18,7 +19,7 @@ const icons = {
   error: <FiAlertCircle size={24} />,
   success: <FiCheckCircle size={24} />,
 };
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -32,7 +33,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [message.id, removeToast]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || "info"]}
       <div>
         <strong>{message.title}</strong>
