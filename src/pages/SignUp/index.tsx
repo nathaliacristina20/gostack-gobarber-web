a0804e31dbeac58d31698d5c879e33fba49bd6cf
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-25 11:35:12 -0300

    Validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import { FiArrowLeft, FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Form } from "@unform/web";
+import * as Yup from "yup";
 import logoImg from "../../assets/logo.svg";
 
 import { Container, Content, Background } from "./styles";
@@ -10,9 +11,24 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Nome obrigatório"),
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().min(6, "No minímo 6 digitos"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
     console.log(data);
-  }
+  }, []);
 
   return (
     <Container>
