4f5aac94cdcd77acadd3198a6e430e8a0c75e6d3
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-19 15:52:26 -0300

    Criando mock da API

---
+++ src/__tests__/hooks/Auth.spec.tsx
@@ -0,0 +1,42 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useAuth, AuthProvider } from '../../hooks/auth';
+import MockAdapter from 'axios-mock-adapter';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+
+describe('Auth hook', () => {
+
+  it('should be able to sign in', async () => {
+
+    const apiResponse = {
+      user: {
+        id: 'user123',
+        name: 'John Doe',
+        email: 'johndoe@example.com'
+      },
+      token: 'token-123'
+    };
+
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+    
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'johndoe@example.com',
+      password: '123456'
+    });
+
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:token', apiResponse.token);
+    expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:user', JSON.stringify(apiResponse.user));
+
+    expect(result.current.user.email).toEqual('johndoe@example.com');
+
+  });
+});
